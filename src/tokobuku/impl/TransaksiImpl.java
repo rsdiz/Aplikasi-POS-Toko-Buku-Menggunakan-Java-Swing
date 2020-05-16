b58ae2e84ea715830b6470b63ec499b63bed96a3
rsdiz <rosyid.iz.rsd@gmail.com>
2020-05-16 07:43:01 +0700

    Add Transaksi Implementation

---
+++ src/tokobuku/impl/TransaksiImpl.java
@@ -0,0 +1,82 @@
+package tokobuku.impl;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import tokobuku.interf.TransaksiInterface;
+import tokobuku.model.Transaksi;
+import tokobuku.util.ConnectionUtil;
+
+/**
+ *
+ * @author Rosyid Iz
+ */
+public class TransaksiImpl implements TransaksiInterface{
+    
+    private final Connection con = ConnectionUtil.getDB();
+
+    @Override
+    public void insert(Transaksi transaksi) throws SQLException {
+        String sql = "CALL insert_transaksi(?,?,?,?,?)";
+        try (PreparedStatement ps = con.prepareStatement(sql)) {
+            ps.setInt(1, transaksi.getId());
+            ps.setInt(2, transaksi.getIdPelanggan());
+            ps.setInt(3, transaksi.getIdPetugas());
+            ps.setString(4, transaksi.getTanggal().toString());
+            ps.setInt(5, transaksi.getNominalBayar().intValue());
+            ps.executeUpdate();
+        }
+    }
+
+    @Override
+    public void update(Transaksi transaksi) throws SQLException {
+        String sql = "CALL update_transaksi(?,?,?,?,?)";
+        try (PreparedStatement ps = con.prepareStatement(sql)) {
+            ps.setInt(1, transaksi.getId());
+            ps.setInt(2, transaksi.getIdPelanggan());
+            ps.setInt(3, transaksi.getIdPetugas());
+            ps.setString(4, transaksi.getTanggal().toString());
+            ps.setInt(5, transaksi.getNominalBayar().intValue());
+            ps.executeUpdate();
+        }
+    }
+
+    @Override
+    public void delete(Transaksi transaksi) throws SQLException {
+        String sql = "CALL delete_transaksi(?)";
+        try (PreparedStatement ps = con.prepareStatement(sql)) {
+            ps.setInt(1, transaksi.getId());
+            ps.executeUpdate();
+        }
+    }
+
+    @Override
+    public List<Transaksi> load() throws SQLException {
+        List<Transaksi> listTransaksi = new ArrayList<>();
+        String sql = "SELECT * FROM data_transaksi";
+        try (Statement statement = con.createStatement()) {
+            ResultSet res = statement.executeQuery(sql);
+            while (res.next()) {
+                Transaksi transaksi = new Transaksi();
+                addTransaksiToList(transaksi, res);
+                listTransaksi.add(transaksi);
+            }
+        }
+        return listTransaksi;
+    }
+    
+    private void addTransaksiToList(Transaksi transaksi, ResultSet res) throws SQLException {
+        transaksi.setId(res.getInt("id_transaksi"));
+        transaksi.setIdPelanggan(res.getInt("id_pelanggan"));
+        transaksi.setIdPetugas(res.getInt("id_kasir"));
+        transaksi.setNamaPelanggan(res.getString("nama_pelanggan"));
+        transaksi.setNamaPetugas(res.getString("nama_petugas"));
+        transaksi.setTanggal(res.getDate("tanggal"));
+        transaksi.setNominalBayar(Double.valueOf(res.getInt("bayar")));
+    }
+    
+}

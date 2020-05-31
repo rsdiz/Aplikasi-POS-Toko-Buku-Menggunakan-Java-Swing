b61dac385c3890f494dfa81442a91eb9d3757c19
rsdiz <rosyid.iz.rsd@gmail.com>
2020-05-31 14:15:39 +0700

    Add ISBN and Byte Image Formatter

--- src/tokobuku/util/Formatter.java
+++ src/tokobuku/util/Formatter.java
@@ -1,14 +1,21 @@
 package tokobuku.util;
 
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.awt.image.WritableRaster;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 import java.text.SimpleDateFormat;
 import java.util.Locale;
+import javax.imageio.ImageIO;
 
 /**
- * Class Generic ini berfungsi untuk
- * membantu pemformatan mata uang maupun tanggal dalam
- * format Negara Indonesia
+ * Class Generic ini berfungsi untuk membantu pemformatan mata uang maupun
+ * tanggal dalam format Negara Indonesia
  *
  * @author Rosyid Iz
  * @param <T>
@@ -16,6 +23,40 @@ import java.util.Locale;
 public class Formatter<T> {
 
     private T t;
+
+    public Formatter() {
+    }
+
+    public static String structIsbn(String isbn) {
+        return String.format("%s-%s-%s-%s-%s",
+                isbn.substring(0, 0 + 3),
+                isbn.substring(3, 3 + 3),
+                isbn.substring(6, 6 + 4),
+                isbn.substring(10, 10 + 2),
+                isbn.substring(12, 12 + 1));
+    }
+
+    public static String destructIsbn(String isbn) {
+        String returnValue = "";
+        String[] arrayIsbn = isbn.split("-");
+        for (String i : arrayIsbn) {
+            returnValue += i;
+        }
+        return returnValue;
+    }
+
+    public static byte[] extractBytes(File fileName) throws IOException {
+        BufferedImage buf = ImageIO.read(fileName);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ImageIO.write(buf, "jpg", baos);
+
+        return baos.toByteArray();
+    }
+    
+    public static BufferedImage buffBytes(byte[] bytes) throws IOException{
+        BufferedImage  buff = ImageIO.read(new ByteArrayInputStream(bytes));
+        return buff;
+    }
 
     public T rupiah(T t) {
         DecimalFormat formatter = (DecimalFormat) DecimalFormat.getCurrencyInstance();
@@ -28,7 +69,7 @@ public class Formatter<T> {
         this.t = (T) formatter.format(t);
         return this.t;
     }
-    
+
     public T tanggal(T t) {
         SimpleDateFormat formatter = new SimpleDateFormat("dd MMMM yyyy", new Locale("id"));
         this.t = (T) formatter.format(t);
